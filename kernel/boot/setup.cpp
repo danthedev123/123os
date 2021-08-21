5501c86894525f0542d0bf4ba65b943729c8ecc2
DanTheDev <danielhammer92@outlook.com>
2021-08-21 07:30:53 +0400

    Add GDT

--- kernel/boot/setup.cpp
+++ kernel/boot/setup.cpp
@@ -1,12 +1,17 @@
 #include "setup.h"
 #include "stivale2/stivale2.h"
 #include "stivale2/stivale2_tags.h"
-#include "tty/tty.h"
+#include "gdt/gdt.h"
 
 void pre_setup(struct stivale2_struct* stivale2_struct)
 {
     InitializeTags(stivale2_struct);
-    tty_init();
+    //tty_init();
 
-    printk("Welcome to \x1b[1;31m1\x1b[1;36m2\x1b[1;32m3\x1b[0mOS!");
+    GDTDescriptor gdtDescriptor;
+
+    gdtDescriptor.size = sizeof(GDT) - 1;
+    gdtDescriptor.offset = (uint64_t)&DefaultGDT;
+
+    loadGDT(&gdtDescriptor);
 }
